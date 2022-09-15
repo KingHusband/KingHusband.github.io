ce44d68c444276e34adf15236092a9bd37fc8e66
KingHusband <105135417+KingHusband@users.noreply.github.com>
2022-09-14 20:46:14 -0400

    v1.0.4

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-let hideTextWs_Icon = document.getElementById('hideTextWs_Icon');
-
-let hideTextWs = document.getElementById('hideTextWs');
-
-hideTextWs_Icon.addEventListener('click', toggleWsText);
-
-function toggleWsText() {
-    hideTextWs.classList.toggle('wsShow');
-}
-
 let hideTextEmail_Icon = document.getElementById('hideTextEmail_Icon');
 
 let hideTextEmail = document.getElementById('hideTextEmail');
